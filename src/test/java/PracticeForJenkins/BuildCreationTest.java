511fecfc60cfea446892cc66632bce411bc9f43d
vijayalaxmibeelagi10@gmail.com <vijayalaxmibeelagi10@gmail.com>
2022-12-05 16:24:50 +0530

    commit-1

---
+++ src/test/java/PracticeForJenkins/BuildCreationTest.java
@@ -0,0 +1,19 @@
+package PracticeForJenkins;
+
+import org.testng.annotations.Test;
+
+public class BuildCreationTest {
+	
+	@Test
+	public void create()
+	{
+		System.out.println("build creation");
+	}
+	
+	@Test
+	public void update()
+	{
+		System.out.println("Build updated");
+	}
+
+}
---
+++ src/test/java/PracticeForJenkins/BuildDeployTest.java
@@ -0,0 +1,19 @@
+package PracticeForJenkins;
+
+import org.testng.annotations.Test;
+
+public class BuildDeployTest {
+	
+	@Test
+	public void sample1()
+	{
+		System.out.println("script-1");
+	}
+	
+	@Test
+	public void sample2()
+	{
+		System.out.println("script-2");
+	}
+
+}
